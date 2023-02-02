4441022e212767bd3e60bb23ee2c346a80c133d5
MrMahendra27 <mkhati47@gmail.com>
2023-02-02 22:42:14 +0530

    added geocode middleware

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoDb = async () => {
+  console.log(process.env.MONGO_URI)
   const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(
     `Connected to database, host: '${conn.connection.host}'.`.blue.underline.bold
--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,113 +1,88 @@
 const Bootcamp = require("../models/Bootcamp");
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/asyncHandler");
 
 /**
  * @desc Get list of all existing bootcamps
  * @route GET /api/v1/bootcamp
  * @access Public
  */
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
 
-    res.status(200).json({
-      status: true,
-      total: bootcamps.length,
-      data: bootcamps,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    status: true,
+    total: bootcamps.length,
+    data: bootcamps,
+  });
+});
 
 /**
  * @desc Get an existing bootcamp
  * @route GET /api/v1/bootcamp/:id
  * @access Public
  */
-exports.getBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findOne({ _id: req.params.id });
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findOne({ _id: req.params.id });
 
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(`Bootcamp of id ${req.params.id} not found!`, 404)
-      );
-    }
-
-    res.status(200).json({
-      status: true,
-      data: bootcamp,
-    });
-  } catch (err) {
-    next(err);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp of id ${req.params.id} not found!`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    status: true,
+    data: bootcamp,
+  });
+});
 
 /**
  * @desc Create new bootcamp
  * @route POST /api/v1/bootcamp
  * @access Public
  */
-exports.createBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
 
-    res.status(201).json({
-      status: true,
-      data: bootcamp,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    status: true,
+    data: bootcamp,
+  });
+});
 
 /**
  * @desc Update existing bootcamp
  * @route PUT /api/v1/bootcamp/:id
  * @access Public
  */
-exports.updateBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(`Bootcamp of id ${req.params.id} not found!`, 404)
-      );
-    }
-
-    res.status(200).json({
-      status: true,
-      data: bootcamp,
-    });
-  } catch (err) {
-    next(err);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp of id ${req.params.id} not found!`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    status: true,
+    data: bootcamp,
+  });
+});
 
 /**
  * @desc Delete existing bootcamp
  * @route DELETE /api/v1/bootcamp/:id
  * @access Public
  */
-exports.deleteBootcamp = async (req, res, next) => {
-  try {
-    await Bootcamp.findByIdAndDelete(req.params.id);
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  await Bootcamp.findByIdAndDelete(req.params.id);
 
-    if (!bootcamp) {
-      return next(
-        new ErrorResponse(`Bootcamp of id ${req.params.id} not found!`, 404)
-      );
-    }
-
-    res.status(204).json({
-      status: true,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(204).json({
+    status: true,
+  });
+});
